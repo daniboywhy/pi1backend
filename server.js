8f9f64e79105b2672171071e74ce970eb1927326
Daniel Costa Gois <danielgois@crea-df.org.br>
2024-09-02 15:24:23 -0300

    jwtsecret

--- server.js
+++ server.js
@@ -21,3 +21,4 @@ app.listen(3000, () => console.log("Server UP^^"));
 //daniboywhy
 //UXhivTxhTcCu1xlU
 //mongodb+srv://daniboywhy:UXhivTxhTcCu1xlU@dbp1.puvmz.mongodb.net/dbp1?retryWrites=true&w=majority&appName=dbp1
+//cbdc5498805a585ad6e8fed76f3ec52526bcca669b21ae29c14faf07b2405971
