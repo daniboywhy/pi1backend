5eeb344d2b6f9ca824118e25252cb7016b58b178
Daniel Góis <danielcostagois@gmail.com>
2024-09-19 02:39:09 -0300

    commit

--- routes/private.js
+++ routes/private.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { PrismaClient } from '@prisma/client';
+import auth from '../middlewares/auth.js';
 
 const router = express.Router();
 const prisma = new PrismaClient();
@@ -13,6 +14,27 @@ router.get('/listar-usuarios', async (req, res) => {
         res.status(500).json({message: "Server Error"})
     }
 })
+
+// Backend: Rota protegida para obter o usuário logado
+router.get('/api/user/me', auth, async (req, res) => {
+    try {
+      // Obtem o ID do usuário a partir do token JWT
+      const userId = req.user.id; // O middleware authenticateToken decodifica o token e anexa o ID ao req.user
+      const user = await prisma.Aluno.findUnique({
+        where: { id: userId }
+      });
+          // Se o usuário não for encontrado, retorna um erro
+      if (!user) {
+        return res.status(404).json({ message: 'Usuário não encontrado' });
+      }
+
+      res.json(user); // Retorna as informações do usuário
+    } catch (error) {
+        console.log(error)
+      res.status(500).json({ message: 'Erro ao buscar informações do usuário' });
+    }
+  });
+  
 
 
 
